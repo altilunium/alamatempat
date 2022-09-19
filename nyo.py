215eb4012efa2809b13fd0a1948dea0cbae7dfa3
Altilunium <70379302+altilunium@users.noreply.github.com>
2022-09-19 19:00:25 +0700

    Add files via upload

---
+++ nyo.py
@@ -0,0 +1,16 @@
+import pickle
+with open('nya.pickle', 'rb') as handle:
+	wordCount = pickle.load(handle)
+
+wordCount = dict(sorted(wordCount.items(), key=lambda item: item[1],reverse=True))			
+
+c = 0
+l = []
+for i in wordCount:
+	if (len(i) < 8):
+		#print(i)
+		c = c + 1
+		l.append(i)
+	#print(i,wordCount[i])
+
+print(l)
